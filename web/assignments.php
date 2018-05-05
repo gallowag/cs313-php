104481945a33e390b9bcd1bcdb39183cd0b85e14
Greer <gallowaygreer@gmail.com>
2018-05-05 16:50:28 -0600

    added background image

--- web/assignments.php
+++ web/assignments.php
@@ -6,10 +6,12 @@
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    
+
+    <link rel="stylesheet" href="style.css">
+
 	<title>Assignments</title>
 </head>
-<body>
+<body class="wallpaper">
 <div class="container">
 	<?php
 
