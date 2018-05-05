7f37c53b4d8f55457ecc1304ea30d41168a6187a
Greer <gallowaygreer@gmail.com>
2018-05-05 10:40:25 -0600

    trying to make function

--- web/index.php
+++ web/index.php
@@ -1,9 +1,6 @@
 <!DOCTYPE html>
-<html>
 <head>
 	<title>Home</title>
-
-	<link rel="stylesheet" href="main.css">
 
 </head>
 <body class="mountain-bg">
@@ -11,10 +8,6 @@
 <div class="container">
 
 <h2>Welcome</h2>
-
-	<div class="jumbotron">
-		<h2>Welcome</h2>
-	</div>
 
 </div>
 
