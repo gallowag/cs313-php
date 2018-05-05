4ca80eea499aa6b6c21a73e30e07eae10a490a68
Greer <gallowaygreer@gmail.com>
2018-05-05 10:43:37 -0600

    adding about me page

---
+++ web/about.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<head>
+	<title>About Me</title>
+</head>
+<body>
+<div class="container">
+
+
+	<div class="jumbotron">
+	<h2>About Us</h2>
+
+	<p>At Make your own Mountain, we believe that everyone should have their own mountain. Whether you are ready for a full-scale mountain, or just getting started with your first hill in the backyard, we have you covered.</p>
+
+	</div>
+</div>
+</body>
+</html>
