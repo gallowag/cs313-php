34e8ff83c95885c14589e2665724705a631e1396
Greer <gallowaygreer@gmail.com>
2018-05-05 10:30:01 -0600

    writing over index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Home</title>
+
+	<link rel="stylesheet" href="main.css">
+
+</head>
+<body class="mountain-bg">
+
+<div class="container">
+
 <?php
-
-  phpinfo();
-
+require("nav.php");
 ?>
+
+	<div class="jumbotron">
+		<h2>Welcome. You are not logged in.</h2>
+	</div>
+
+</div>
+
+</body>
+</html>
