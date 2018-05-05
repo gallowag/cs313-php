27c8e7bdc7586658cf2fe1f019dd3510b7f484eb
Greer <gallowaygreer@gmail.com>
2018-05-05 11:42:32 -0600

    changing nav bar

--- web/nav.php
+++ web/nav.php
@@ -10,12 +10,12 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 	<ul class="nav navbar-nav">
 
 
-		<li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
-			<a href="about.php">About Us</a>
-		</li>
-
 		<li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
 			<a href="index.php">Home</a>
+		</li>
+
+		<li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
+			<a href="about.php">About Us</a>
 		</li>
 
 		<li class="nav-item <?php if ($file === 'assignments') echo 'active' ?>">
