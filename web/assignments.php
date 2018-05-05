ef934a029ad346b85a90a7a0bc01f6a27c82772d
Greer <gallowaygreer@gmail.com>
2018-05-05 10:48:04 -0600

    adding assignments page

---
+++ web/assignments.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+	<title>Assignments</title>
+</head>
+<body>
+<div class="container">
+
+
+	<div class="jumbotron">
+	<h2>Coming soon!</h2>
+
+	</div>
+</div>
+</body>
+</html>
