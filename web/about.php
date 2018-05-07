acd145eed19ac4362087291c8ab74944226ffcf4
Greer <gallowaygreer@gmail.com>
2018-05-07 06:44:30 -0600

    trying grid system

--- web/about.php
+++ web/about.php
@@ -19,10 +19,15 @@
 	?>
 	<div class="jumbotron">
 
+	<div class="col-md-4">
 	<img src="me.jpg" class="img-thumbnail" alt="Me">
+	</div>
+
+	<div class="col-md-8">
 	<h2>About Me</h2>
 
 	<p>Hello! I'm a student at BYU-I studying computer science. I love to read, draw, listen to k-pop, and dance! While I am studying most of the time I am also volunteering as a dance instructor on campus teaching swing. I'm from Kent, Washington which is about a half-hour south of Seattle. I'm the oldest of five kids and I love my family to death. Just over a year ago, my family moved to Rexburg and I moved in with them to save money. Being neighbors with the temple has been a blessing in our lives.</p>
+	</div>
 
 	</div>
 </div>
