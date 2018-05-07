dfe673c20cebd7b74ef4820ab1def1d228028b88
Greer <gallowaygreer@gmail.com>
2018-05-07 07:37:37 -0600

    navbar not working

--- web/nav.php
+++ web/nav.php
@@ -6,22 +6,22 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
 <h1>Greer Galloway</h1>
 
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-inverse">
 	<div class="container-fluid">
 	<ul class="nav navbar-nav">
 
-
-		<li class="nav-item <?php if ($file === 'home') echo 'active' ?>">
+		<li class="nav-item<?php if ($file === 'home') echo 'active' ?>">
 			<a href="index.php">Home</a>
 		</li>
 
-		<li class="nav-item <?php if ($file === 'about') echo 'active' ?>">
+		<li class="nav-item<?php if ($file === 'about') echo 'active' ?>">
 			<a href="about.php">About Me</a>
 		</li>
 
-		<li class="nav-item <?php if ($file === 'assignments') echo 'active' ?>">
+		<li class="nav-item<?php if ($file === 'assignments') echo 'active' ?>">
 			<a href="assignments.php">Assignments</a>
 		</li>
+		
 	</ul>
 	</div>
 </nav>
