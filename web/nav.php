4666dc312598ac4d96ce022297fbdf31c6ddee86
Greer <gallowaygreer@gmail.com>
2018-05-07 07:31:44 -0600

    fixing nav?

--- web/nav.php
+++ web/nav.php
@@ -6,7 +6,8 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
 <h1>Greer Galloway</h1>
 
-<nav class="navbar navbar-inverse">
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
 	<ul class="nav navbar-nav">
 
 
@@ -21,5 +22,6 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 		<li class="nav-item <?php if ($file === 'assignments') echo 'active' ?>">
 			<a href="assignments.php">Assignments</a>
 		</li>
-	<ul>
+	</ul>
+	</div>
 </nav>
