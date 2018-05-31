c5b29c930d2ed5dd0af85f1bb8804f26c15d0f92
Greer <gallowaygreer@gmail.com>
2018-05-31 15:41:19 -0600

    adding courses

---
+++ web/week06/courses.php
@@ -0,0 +1,19 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses</title>
+</head>
+<body>
+	<h1>Courses</h1>
+	<ul>
+		<li><p>Course 1</p></li>
+		<li><p>Course 2</p></li>
+		<li><p>Course 3</p></li>
+		<li><p>Course 4</p></li>
+	</ul>
+</body>
+</html>
