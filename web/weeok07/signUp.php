eeac6e1cb23540783dd50f7da579b4a6548b0bce
Greer <gallowaygreer@gmail.com>
2018-06-05 16:14:47 -0600

    starting

---
+++ web/weeok07/signUp.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Courses</title>
+</head>
+<body>
+	<h1>Sign up!</h1>
+	<form action="create.php" method="post">
+		username:<input type="text" name="username">
+		password:<input type="text" name="password">
+		<button type="submit">Submit</button>
+	</form>
+
+</body>
+</html>
