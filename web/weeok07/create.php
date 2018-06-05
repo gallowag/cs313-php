474a49aefe83cb0445ad1a2325e620c69f398bdc
Greer <gallowaygreer@gmail.com>
2018-06-05 16:26:08 -0600

    starting

--- web/weeok07/create.php
+++ web/weeok07/create.php
@@ -1,4 +1,24 @@
 <?php
 
-echo "<h1>hello</h1>";
+$pass = $_POST["password"];
+$user = $_POST["username"];
+
+$hash = password_hash($pass, PASSWORD_DEFAULT);
+
+//connect to database
+require("dbConnect.php");
+
+$db = get_db();
+if (!isset($db)) {
+	die("DB Connection was not set");
+}
+
+$stmt = $db->prepare("INSERT INTO person (username,password) VALUES (:username,:password)");
+$stmt->bindValue(":username",$user,PDO::PARAM_STR);
+$stmt->bindValue(":password",$hash,PDO::PARAM_STR);
+$stmt->execute();
+
+header("Location: welcome.php");
+exit;
+
 ?>
