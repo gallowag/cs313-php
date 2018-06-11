3ab7b0547820371a94f65544c577f496b0c4da5e
Greer <gallowaygreer@gmail.com>
2018-06-11 07:50:19 -0600

    trying to add logged in at the top

--- web/project01/nav.php
+++ web/project01/nav.php
@@ -2,6 +2,8 @@
 
 $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 
+
+
 ?>
 
 <br>
@@ -24,5 +26,14 @@ $file = pathinfo($_SERVER['PHP_SELF'], PATHINFO_FILENAME);
 		</li>
 		
 	</ul>
+	<?php
+		session_start();
+		if(isset($_SESSION['id'])) {
+			$id = $_SESSION['id'];
+			echo "<ul class="nav navbar-nav navbar-right"><li>Welcome, $id</li></ul>";
+		} else {
+			echo "<ul class="nav navbar-nav navbar-right"><li><a href=\"home.php\">Sign In</a></li></ul>";
+		}	
+	?>
 	</div>
 </nav>
