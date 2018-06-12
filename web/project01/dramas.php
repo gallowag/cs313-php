92dcf5a02f72d96ee494f1b40b31555c205739a2
Greer <gallowaygreer@gmail.com>
2018-06-12 15:25:49 -0600

    styling create user form

--- web/project01/dramas.php
+++ web/project01/dramas.php
@@ -60,7 +60,7 @@ $dramas = $statement->fetchAll(PDO::FETCH_ASSOC);
 
 		$drama_rating = $row6["drama_rating"];
 
-		echo "<li class=\"col-sm-6\"><a href='dramaDetails.php?drama_id=$id'><img src='$img' class='img-thumbnail'><p>$title | $drama_rating</p></a></li>";
+		echo "<li><a href='dramaDetails.php?drama_id=$id'><img src='$img' class='img-thumbnail'><p>$title | $drama_rating</p></a></li>";
 
 	}
 	?>
