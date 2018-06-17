9796d24ca0784dfb995c5f65d62bd0685558efa2
Greer <gallowaygreer@gmail.com>
2018-06-16 20:31:46 -0600

    changed some style things

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -7,8 +7,11 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
 </head>
-<body>
+<body class="space-bg">
+
 	<div class="container">
+	<div class="jumbotron">
+
 	<h3>Shopping Cart</h3>
 	<?php
 	session_start();
@@ -22,8 +25,9 @@
 	?>
 
 	<br/>
-	<button><a href="browse.html">Continue Shopping</a></button>
-	<button><a href="checkout.php">Go to check out</a></button>
+	<form action="browse.html"><button type="submit" class="btn btn-primary">Continue Shopping</button></form>
+	<form action="checkout.php"><button type="submit" class="btn btn-primary">Go to check out</button></form>
+	</div>
 	</div>
 
 </body>
