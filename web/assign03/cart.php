493c89a175aeb018d60a8098f1f35c38f5360451
Greer <gallowaygreer@gmail.com>
2018-06-18 13:46:31 -0600

    changed some style things

--- web/assign03/cart.php
+++ web/assign03/cart.php
@@ -6,10 +6,13 @@
 	  <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
 
+    <link rel="stylesheet" href="style.css">
+
 </head>
 <body class="space-bg">
-
 	<div class="container">
+	<h1>Star Wars Lego</h1>
+
 	<div class="jumbotron">
 
 	<h3>Shopping Cart</h3>
@@ -25,7 +28,7 @@
 	?>
 
 	<br/>
-	<form action="browse.html"><button type="submit" class="btn btn-primary">Continue Shopping</button></form>
+	<form action="browse.html"><button type="submit" class="btn btn-primary">Continue Shopping</button></form><br>
 	<form action="checkout.php"><button type="submit" class="btn btn-primary">Go to check out</button></form>
 	</div>
 	</div>
